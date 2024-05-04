779b170a8b2276afb43d533ba470ae059d19057d
AvishekShaw <47551320+AvishekShaw@users.noreply.github.com>
2024-05-04 15:26:04 +0530

    updated android

--- Obsidian/nsync.sh
+++ Obsidian/nsync.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-SHARED="data/data/com.termux/files/home/storage/shared"
+SHARED="/data/data/com.termux/files/home/storage/shared"
 cd $SHARED/Obsidian-notes
 
 find . -type f \( -name "*.md" -o -name "*.png"  -o -name "*.pdf" \) -print0 | xargs -0 git addgit ls-files --deleted -z | xargs -0 git add
