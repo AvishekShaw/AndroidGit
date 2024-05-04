1f018f60a6d92c19b0d5e4f1e4b221084ca2bacb
AvishekShaw <47551320+AvishekShaw@users.noreply.github.com>
2024-05-04 15:30:45 +0530

    updated android

--- Obsidian/nsync.sh
+++ Obsidian/nsync.sh
@@ -2,7 +2,8 @@
 SHARED="/data/data/com.termux/files/home/storage/shared"
 cd $SHARED/Obsidian-notes
 
-find . -type f \( -name "*.md" -o -name "*.png"  -o -name "*.pdf" \) -print0 | xargs -0 git addgit ls-files --deleted -z | xargs -0 git add
+find . -type f \( -name "*.md" -o -name "*.png"  -o -name "*.pdf" \) -print0 | xargs -0 git add
+git ls-files --deleted -z | xargs -0 git add
 
 git commit -m "android on $(date)"
 git pull origin master
